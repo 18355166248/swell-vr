1d8b4318ea2e5f27cb4921ae0bab0aef93b76a96
lang.jiang <lang.jiang@ximalaya.com>
2024-11-19 20:05:22 +0800

    feat: 2

--- packages/threejs-demo/src/examples/Study/02-几何体BufferGeometry/index.tsx
+++ packages/threejs-demo/src/examples/Study/02-几何体BufferGeometry/index.tsx
@@ -1,6 +1,5 @@
 import {useLayoutEffect, useRef} from 'react'
 import * as THREE from 'three'
-import * as dat from 'dat.gui'
 import ThreeBase from '../../../utils/ThreeBase'
 
 function Two() {
@@ -10,20 +9,11 @@ function Two() {
     if (!canvas.current) return
 
     class MyThree extends ThreeBase {
-      material?: THREE.ShaderMaterial
-      group?: THREE.Group<THREE.Object3DEventMap>
-      normalObj?: THREE.Mesh<
-        THREE.ExtrudeGeometry,
-        THREE.MeshBasicMaterial,
-        THREE.Object3DEventMap
-      >
-      time: number
       constructor() {
         super()
         this.isControl = true
         this.isStats = true
         this.isGui = true
-        this.time = 1.0
         this.dataObj = {
           color: '#00ff00',
           x: 0,
@@ -73,7 +63,50 @@ function Two() {
           },
         ]
       }
-      createChart() {}
+      createChart() {
+        //创建一个空的几何体对象
+        const geometry = new THREE.BufferGeometry()
+        //类型化数组创建顶点数据
+        const vertices = new Float32Array([
+          0,
+          0,
+          0, //顶点1坐标
+          50,
+          0,
+          0, //顶点2坐标
+          0,
+          100,
+          0, //顶点3坐标
+          0,
+          0,
+          10, //顶点4坐标
+          0,
+          0,
+          100, //顶点5坐标
+          50,
+          0,
+          10, //顶点6坐标
+          0,
+          0,
+          0,
+        ])
+        // 创建属性缓冲区对象
+        //3个为一组，表示一个顶点的xyz坐标
+        const attribute = new THREE.BufferAttribute(vertices, 3)
+        // 设置几何体attributes属性的位置属性
+        geometry.attributes.position = attribute
+
+        this.scene?.add(
+          new THREE.Line(
+            geometry,
+            new THREE.MeshBasicMaterial({
+              color: this.dataObj.color,
+            }),
+          ),
+        )
+
+        this.camera?.position.set(150, 300, 250)
+      }
     }
 
     const myThree = new MyThree()
